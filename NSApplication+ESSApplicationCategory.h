7bd54aa0d9d849f0c8eb37cbf32a38fe592e93b9
Matthias Gansrigler <opensource@eternalstorms.at>
2012-10-01 20:33:02 +0200

    initial commit

---
+++ NSApplication+ESSApplicationCategory.h
@@ -0,0 +1,49 @@
+//
+//  NSApplication+ESSApplicationCategory.h
+//  flickery
+//
+//  Created by Matthias Gansrigler on 01.10.12.
+//  Copyright (c) 2012 Eternal Storms Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSApplication (ESSApplicationCategory)
+
+void ESSBeginAlertSheet(NSString *title,
+						NSString *defaultButton,
+						NSString *alternateButton,
+						NSString *otherButton,
+						NSWindow *window,
+						void (^didEndBlock)(void *contextInf,
+											NSInteger returnCode),
+						void (^didDismissBlock)(void *contextInf,
+												NSInteger returnCode),
+						void *contextInfo,
+						NSString *formattedString);
+
+void ESSInformationalBeginAlertSheet(NSString *title,
+									 NSString *defaultButton,
+									 NSString *alternateButton,
+									 NSString *otherButton,
+									 NSWindow *window,
+									 void (^didEndBlock)(void *contextInf,
+														 NSInteger returnCode),
+									 void (^didDismissBlock)(void *contextInf,
+															 NSInteger returnCode),
+									 void *contextInfo,
+									 NSString *formattedString);
+
+void ESSCriticalBeginAlertSheet(NSString *title,
+								NSString *defaultButton,
+								NSString *alternateButton,
+								NSString *otherButton,
+								NSWindow *window,
+								void (^didEndBlock)(void *contextInf,
+													NSInteger returnCode),
+								void (^didDismissBlock)(void *contextInf,
+														NSInteger returnCode),
+								void *contextInfo,
+								NSString *formattedString);
+
+@end
